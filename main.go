76332eb06cf80237883e16555bb46532988250e9
Marcos Minond <minond.marcos@gmail.com>
2017-09-18 12:53:03 -0600

    uploading files

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -13,10 +15,41 @@ func main() {
 	}
 
 	http.HandleFunc("/ping", ping)
+	http.HandleFunc("/upload", upload)
 	http.ListenAndServe(":"+port, nil)
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "plain/text")
 	w.Write([]byte("pong"))
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Unsupported method", http.StatusNotFound)
+		return
+	}
+
+	r.ParseMultipartForm(32 << 20)
+	tmpFile, handler, err := r.FormFile("file")
+
+	if err != nil {
+		msg := fmt.Sprintf("Could not extract file from request: %s", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+
+	defer tmpFile.Close()
+	fmt.Fprintf(w, "%v", handler.Header)
+
+	localFile, err := os.OpenFile("./uploads/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+
+	if err != nil {
+		msg := fmt.Sprintf("Could not access internal storage: %s", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+
+	defer localFile.Close()
+	io.Copy(localFile, tmpFile)
 }
