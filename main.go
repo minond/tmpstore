f292735cee08abf142f1b42d047f4d2a92fd33be
Marcos Minond <minond.marcos@gmail.com>
2017-09-18 23:32:47 -0600

    cors header

--- main.go
+++ main.go
@@ -65,6 +65,7 @@ func upload(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write([]byte(fmt.Sprintf(`{"name":"%s"}`, name)))
 }
 
